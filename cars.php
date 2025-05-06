898e396c79f718a5acc2b1bcbb4c656554d1de1a
105178766 <105178766@student.swin.edu.au>
2025-05-06 21:31:08 +1000

    fixed html validation by adding internal css fromatting

--- cars.php
+++ cars.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 9</title>
+
+    <style>
+.car-table {
+    border-collapse: collapse; /* removes white space between table cells */  
+}
+.car-table th, /*table header cells*/
+.car-table td { /*table data cells*/
+    border: 1px solid black;
+    padding: 5px;
+    text-align: center;
+}
+</style>
+
+</head>
+<body>
+   
 <?php
 require_once "settings.php";
 $dbconn = @mysqli_connect ($host,$user,$pwd,$sql_db); //connecting to the database
@@ -9,7 +31,7 @@ else {
     $result = mysqli_query($dbconn, $query); //execute and store the result of the query
     if($result){ //check if the query was true / successful
         if (mysqli_num_rows($result) > 0) {
-            echo "<table border='5'>";
+            echo "<table class='car-table'>"; //create a table to display the results
             echo "<tr><th>Car ID</th><th>Make</th><th>Model</th><th>Price</th><th>Year of Manufacture</th></tr>"; //Genai"Help me with creating this table"
             
             //print each row in the table
@@ -34,3 +56,6 @@ else {
 }
 mysqli_close ($dbconn); //best practise to close / free up resources
 ?>
+
+</body>
+</html>
